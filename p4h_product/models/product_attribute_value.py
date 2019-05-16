a1a28c7aa526b1b47280513d2a17d0b9f694deb6
Ray Carnes <ray@odoo.com>
2019-05-16 19:29:36 +0000

    Startover

--- p4h_product/models/product_attribute_value.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-
-from odoo import models, fields, api, _
-
-class ProductAttributeValue(models.Model):
-    _inherit = 'product.attribute.value'
-
-    source_doc = fields.Char('MIF Source')
-
--- p4h_product/models/product_public_category.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-
-from odoo import models, fields, api, _
-
-class ProductPublicCategory(models.Model):
-    _inherit = 'product.public.category'
-
-    categ_id = fields.Integer(string='MIF Category ID')
-
