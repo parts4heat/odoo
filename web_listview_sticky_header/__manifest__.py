5c15d464f9714dda51cbaf982c35f4c323c4e469
Your Name <you@example.com>
2019-05-20 15:36:12 +0000

    Another module

---
+++ web_listview_sticky_header/__manifest__.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "Web List View Sticky Header",
+
+    'summary': """
+      Web List View Sticky Header v12.0""",
+
+    'description': """
+        
+    """,
+
+    'author': "Ksolves",
+    'website': "https://www.ksolves.com/",
+    'license': 'LGPL-3',
+    'category': 'Tools',
+    'version': '1.0.2',
+    'images': ['static/description/main.png'],
+    'depends': ['base'],
+
+    'data': [
+        'views/assets.xml',
+    ],
+
+}
