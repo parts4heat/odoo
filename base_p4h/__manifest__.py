cecfc8b6ab2494272e0a36785f8c3c902ec1a933
Jose M. Coronado <j.coronado.personal@gmail.com>
2019-05-23 10:36:58 -0700

    [IMP] Manifest version change

--- base_p4h/__manifest__.py
+++ base_p4h/__manifest__.py
@@ -11,7 +11,7 @@
     "category": "Hidden",
     "author": "GFP Solutions LLC",
     "summary": "Custom",
-    "version": "1.0",
+    "version": "1.2",
     "description": """
 
 THIS MODULE IS PROVIDED AS IS - INSTALLATION AT USERS' OWN RISK - AUTHOR OF MODULE DOES NOT CLAIM ANY
