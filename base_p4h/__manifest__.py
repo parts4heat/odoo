5a9570696c64fa0440f7b6daafd324f5da306e33
Jose M. Coronado <j.coronado.personal@gmail.com>
2019-06-10 21:24:43 -0700

    [IMP] Forcing Restart with manifest change

--- base_p4h/__manifest__.py
+++ base_p4h/__manifest__.py
@@ -11,7 +11,7 @@
     "category": "Hidden",
     "author": "GFP Solutions LLC",
     "summary": "Custom",
-    "version": "1.3",
+    "version": "3.0",
     "description": """
 
 THIS MODULE IS PROVIDED AS IS - INSTALLATION AT USERS' OWN RISK - AUTHOR OF MODULE DOES NOT CLAIM ANY
