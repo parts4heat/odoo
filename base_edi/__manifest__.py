5bf04a23d591127fe167df7f360a0d802e421338
Darshan Kalola <dka@odoo.com>
2019-06-19 14:46:37 +0530

    [ADD]added basic EDI connection module

---
+++ base_edi/__manifest__.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+
+
+{
+    'name': 'EDI Document Synchonizaton Base',
+    'version': '1.0',
+    'category': 'Tools',
+    'description': """
+Allows you to configure edi document exchnage configurations
+==============================================================
+You can your own edi xml export and import via ftp.
+""",
+    'depends': ['mail'],
+    'data': [
+        'security/base_edi_security.xml',
+        'security/ir.model.access.csv',
+        'views/edi_config_view.xml',
+    ],
+    'demo': [],
+    'installable': True,
+}
---
+++ base_edi/models/__init__.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+
+from . import ftp_connection
+from . import sftp_connection
+from . import edi_config
+#from . import res_partner
